458e9a6a89fb554299e819184392d85036c686e0
Temuujin <Temuujingv@gmail.com>
2023-10-17 00:18:03 +0800

    updated header to responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
 import logo from '../../img/Modern Sys_CoLogo.png'
+import { CloseOutlined, MenuOutlined } from '@ant-design/icons'
 
 function Header() {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => {
+    setClick(!click)
+  }
+
+  const handleClose = () => {
+    setClick(false)
+  }
+
   return (
-       <div className='header'>
-        <img src={logo} width={90} height={90} alt='syscotech'/>
-        <ul class="nav-links">
-          <li class="upward"><a href="/">Dashboard</a></li>
-          <li class="upward"><Link to='/'>Portfolio</Link></li>
-          <li class="upward"><a href="/">Services</a></li>
-          <li class="upward"><Link to='/mainformsecond'>olympad burtguuleh</Link></li>
-        </ul>
+    <div className='header'>
+      <img src={logo} width={90} height={90} alt='syscotech'/>
+      <ul className={`nav-links ${click ? 'active' : ''}`}>
+        <li className="upward"><Link to='/' onClick={handleClose}>Dashboard</Link></li>
+        <li className="upward"><Link to='/portfolio' onClick={handleClose}>Portfolio</Link></li>
+        <li className="upward"><Link to='/services' onClick={handleClose}>Services</Link></li>
+        <li className="upward"><Link to='/mainformsecond' onClick={handleClose}>olympad burtguuleh</Link></li>
+      </ul>
+      <div className='hamburger' onClick={handleClick}>
+        {click ? <CloseOutlined className='icon' /> : <MenuOutlined className='icon' /> }
       </div>
+    </div>
   )
 }
 
